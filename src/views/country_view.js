debc8b8a4de3b1adfd99d5297c3cf014362f9445
Joseph Young <jsyoung0891@gmail.com>
2019-05-19 21:30:17 +0100

    map movement is working but only once!!!

--- src/views/country_view.js
+++ src/views/country_view.js
@@ -11,6 +11,7 @@ CountryView.prototype.bindEvents = function(){
         
         console.log('should be all of a countrys data?', event.detail);
         this.render(event.detail)
+        this.renderMap(event.detail)
         
     })
 }
@@ -24,13 +25,26 @@ CountryView.prototype.render = function(countryInfo){
     this.container.appendChild(countryName)
 
     
+
+
+   
+}
+
+CountryView.prototype.renderMap = function(countryInfo){
     const countryLatitude = countryInfo.latlng[0]
     const countryLongitude = countryInfo.latlng[1]
     console.log('latlng: ', countryLatitude, countryLongitude)
 
-    
+    var mymap = L.map('mapid').setView([countryLatitude,
+        countryLongitude], 6);
 
-   
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox.streets'
+  }).addTo(mymap);
 }
 
 
