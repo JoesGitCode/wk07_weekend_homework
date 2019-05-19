aca30e47e1030eaef2edeb51a8ab8063aeed1098
Joseph Young <jsyoung0891@gmail.com>
2019-05-19 21:20:17 +0100

    got a map, it doesnt do much yet...

--- src/views/country_view.js
+++ src/views/country_view.js
@@ -16,9 +16,21 @@ CountryView.prototype.bindEvents = function(){
 }
 
 CountryView.prototype.render = function(countryInfo){
+
+    this.container.innerHTML = ''
+
     const countryName = document.createElement('h2')
     countryName.textContent = `Name: ${countryInfo.name}`
     this.container.appendChild(countryName)
+
+    
+    const countryLatitude = countryInfo.latlng[0]
+    const countryLongitude = countryInfo.latlng[1]
+    console.log('latlng: ', countryLatitude, countryLongitude)
+
+    
+
+   
 }
 
 
