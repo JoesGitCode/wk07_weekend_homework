521ac51b09dd22f5f5281b1935e2b16dbee50add
Joseph Young <jsyoung0891@gmail.com>
2019-05-19 17:14:21 +0100

    populated the dropdown

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -7,17 +7,28 @@ const SelectView = function(element){
 SelectView.prototype.bindEvents = function(){
     PubSub.subscribe('Countries:all-countries', (event) => {
         console.log('SelectView subscribed to Countries:all-countries');
+        const allCountriesInfo = event.detail
         console.log(event.detail);
+        this.getEachCountryName(allCountriesInfo)
         
     this.element.addEventListener('change', (event) => {
-        selectedIndex = event.target.value
-        console.log(selectedIndex);
+        selectedCountryName = event.target.value
+        console.log('selected country name: ', event.target.value);
 
-        PubSub.publish('SelectView:single-country-select', selectedIndex)
+        PubSub.publish('SelectView:single-country-select', selectedCountryName)
         console.log('SelectView published SelectView:single-country-select');
         
         
     })        
+    })
+
+}
+
+SelectView.prototype.getEachCountryName = function(countries){
+    countries.forEach((country) => {
+        const option = document.createElement('option')
+        option.textContent = country.name
+        this.element.appendChild(option)
     })
 }
 
